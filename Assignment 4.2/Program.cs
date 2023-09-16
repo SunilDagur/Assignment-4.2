c842c1a01baaf4b7c696546e0cd04e844dab8616
Sunil Dagur <sunildagur566@gmail.com>
2023-09-16 18:34:54 +0530

    Add project files.

---
+++ Assignment 4.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4._2
+{
+    using System;
+
+    class Car
+    {
+        
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+       
+        public string FullCarName
+        {
+            get
+            {
+                return $"{Make} {Model} {Year}";
+            }
+        }
+
+       
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        
+        public static void Main(string[] args)
+        {
+            Car myCar = new Car("Toyota", "Camry", 2022);
+            Console.WriteLine($"Full Car Name: {myCar.FullCarName}");
+        }
+    }
+
+}
